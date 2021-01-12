82bc78bc9368a7d8d598c549b85eed7791098696
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-11 21:14:10 -0600

    clase 8 recibir informacion desde el cliente

--- backendNodeJS/server.js
+++ backendNodeJS/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const { json } = require('express');
+
 const port = 8080;
+
 let app = express();
 
 app.use(router);
+app.use(bodyParser,json);
+
 router.get('/message', function (req,res){
-      res.send('Hola desde get')
+      res.send('Hola desde get');
 });
+
 router.post('/message', function (req,res){
-      res.send('Hola desde post')
+      res.send('Hola desde post');
 });
+
 router.delete('/message', function (req,res){
-      res.send('Hola desde delete')
+      console.log(req.query);
+      console.log(req.body)
+      res.send('mensaje ' + req.body.text + ' añadido correctamente...')
+
 });
+
 // app.use('/', function (req,res){
 //       res.send('Hola');
 // });
