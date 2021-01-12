3f13bc44699db7f8a7bacae750d1a4f2f2794611
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-11 20:49:34 -0600

    clase 7 ¿Cómo pueden venir las peticiones?

--- backendNodeJS/server.js
+++ backendNodeJS/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 const port = 8080;
 let app = express();
 
-app.use('/', function (req,res){
-      res.send('Hola');
+app.use(router);
+router.get('/message', function (req,res){
+      res.send('Hola desde get')
 });
+router.post('/message', function (req,res){
+      res.send('Hola desde post')
+});
+router.delete('/message', function (req,res){
+      res.send('Hola desde delete')
+});
+// app.use('/', function (req,res){
+//       res.send('Hola');
+// });
 
 app.listen(port)
 console.log('la aplicacion esta escuchando en el puerto: '+ port)
