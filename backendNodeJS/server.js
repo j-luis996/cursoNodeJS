5912bc6c5149170b40d612c9865a62f8b1216b0b
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-12 17:26:10 -0600

    clase: Respuestas coherentes

--- backendNodeJS/server.js
+++ backendNodeJS/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const response = require('./network/response');
 
 const port = 8080;
 
@@ -16,23 +17,31 @@ router.get('/message', function (req,res){
       res.header({
             "custom-herder": "valor personalizado"
       });
-      res.send('Hola desde get');
+      // res.send('Hola desde get');
+      response.success(req,res,'Lista de mensajes');
 });
 
 router.post('/message', function (req,res){
-      res.send('Hola desde post');
-});
-
-router.delete('/message', function (req,res){//esto sera mas de post pero equisde
       console.log(req.query);
-      console.log(req.body);
-      // res.send('mensaje '+ req.body.text+' correctamente...');
-      res.status(201).send({error: "",body: 'Creado correctamente'});
+      if(req.query.error == 'ok'){
+            response.error(req,res,'error simulado',401);
+      }else{
+            response.success(req,res,'lista de mensajes',201);
+      }
+      // res.status(201).send({error: "",body: 'Creado correctamente'});
+      
 });
 
-//app.use('/', function (req,res){
-//       res.send('Hola');
+// router.delete('/message', function (req,res){//esto sera mas de post pero equisde
+//       console.log(req.query);
+//       console.log(req.body);
+//       res.send('mensaje '+ req.body.text+' correctamente...');
+//       res.status(201).send({error: "",body: 'Creado correctamente'});
 // });
+
+app.use('/', function (req,res){
+      res.send('Hola');
+});
 
 app.listen(port)
 console.log('la aplicacion esta escuchando en el puerto: '+ port)
