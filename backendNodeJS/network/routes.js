9fc2890bae4ba45b1265e39e3ccbcea8ab03d8c2
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-13 17:52:43 -0600

    agregué la ruta /app para servir estaticos

--- backendNodeJS/network/routes.js
+++ backendNodeJS/network/routes.js
@@ -3,6 +3,7 @@ const message = require('../components/messages/network');
 
 const routes = function (server){
       server.use('/message',message);
+      server.use('/app', express.static('public'));
 }
 
 module.exports = routes;
--- backendNodeJS/server.js
+++ backendNodeJS/server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// const router = require('./components/messages/network');
 const router =require('./network/routes');
+
 const port = 8080;
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(router);
 
 router(app);
+// app.use('/app', express.static('public'));
+app.listen(port);
 
-app.use('/app', express.static('public'));
-app.listen(port)
-console.log('la aplicacion esta escuchando en el puerto: '+ port)
+console.log('la aplicacion esta escuchando en el puerto: '+ port);
